501b20332db16c2621ea1c6890e8e53d870d70eb
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2020-04-20 13:24:55 -0400

    April 20 commit

--- Contact.cs
+++ Contact.cs
@@ -14,36 +14,50 @@ namespace _205704Unit2Summative
         public int monthBorn { get; set; }
         public int dayBorn { get; set; }
         public string email { get; set; }
-        private int age;
+        public int age;
 
-        public Contact (string fileLine)
+        public Contact(string fileLine)
         {
             parseLine(fileLine);
         }
-        private void parseLine (string unparsedLine)
+        private void parseLine(string unparsedLine)
         {
-            string Line = unparsedLine;
-            firstName = Line.Substring(0, Line.IndexOf(","));
-            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            lastName = Line.Substring(0, Line.IndexOf(","));
-            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int yB);
-            yearBorn = yB;
-            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int mB);
-            monthBorn = mB;
-            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int dB);
-            dayBorn = dB;
-            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            email = Line.Substring(0, Line.IndexOf(","));
+            string[] data = unparsedLine.Split(",");
+            //string Line = unparsedLine;
+            //firstName = Line.Substring(0, Line.IndexOf(","));
+            firstName = data[0];
+            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            //lastName = Line.Substring(0, Line.IndexOf(","));
+            lastName = data[1];
+            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            //int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int yB); 
+            int.TryParse(data[2], out int yB); yearBorn = yB;
+            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            //int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int mB); 
+            int.TryParse(data[3], out int mB); monthBorn = mB;
+            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            //int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int dB); 
+            int.TryParse(data[4], out int dB); dayBorn = dB;
+            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            //email = Line;
+            email = data[5];
+
+            //SetAge();
         }
         private void SetAge()
         {
             DateTime birthDay = new DateTime(yearBorn, monthBorn, dayBorn);
             DateTime dateToday = DateTime.Today;
             TimeSpan TSage = dateToday - birthDay;
-            age = (TSage.Days) / 1461 / 4;
+            age = (TSage.Days) / 1461 * 4;
+        }
+
+        public override string ToString()
+        {
+            string line = firstName + "," + lastName + "," +
+                yearBorn.ToString() + "," + monthBorn.ToString() + "," +
+                dayBorn.ToString() + "," + email + '\r';
+            return line;
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,18 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows;
-using System.Windows.Controls;
-using System.Windows.Data;
-using System.Windows.Documents;
-using System.Windows.Input;
-using System.Windows.Media;
-using System.Windows.Media.Imaging;
-using System.Windows.Navigation;
-using System.Windows.Shapes;
 using System.IO;
+using Microsoft.Win32;
 
 namespace _205704Unit2Summative
 {
@@ -26,14 +16,16 @@ namespace _205704Unit2Summative
         {
             InitializeComponent();
             contacts = new List<Contact>();
-            ReadFromFile("contact.txt");
+            ReadFromFile("contacts.txt");
+
+            dgEditor.ItemsSource = contacts;
         }
 
         private void menuOpen_Click(object sender, RoutedEventArgs e)
         {
             Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
             bool? dialogResult = ofd.ShowDialog();
-            if ((bool)ofd.ShowDialog())
+            if ((bool)dialogResult)
             {
                 ReadFromFile(ofd.FileName);
             }
@@ -41,7 +33,12 @@ namespace _205704Unit2Summative
 
         private void menuSave_Click(object sender, RoutedEventArgs e)
         {
-
+            SaveFileDialog sfd = new SaveFileDialog();
+            bool? fileSelected = sfd.ShowDialog();
+            if ((bool)fileSelected)
+            {
+                SavetoFile(sfd.FileName);
+            }
         }
 
         public void ReadFromFile(String fileURI)
@@ -55,12 +52,60 @@ namespace _205704Unit2Summative
                     Contact temp = new Contact(line);
                     contacts.Add(temp);
                 }
+                sr.Close();
             }
             catch (Exception)
             {
                 MessageBox.Show("Error Reading file at URI: " + fileURI);
                 throw;
             }
+            dgEditor.ItemsSource = null;
+            dgEditor.ItemsSource = contacts;
+        }//end read from file method
+
+        public void SavetoFile(string fileName)
+        {
+            string save = "";
+            foreach(Contact c in contacts)
+            {
+                save += c.ToString();
+            }
+
+            StreamWriter sw = new StreamWriter(fileName);
+            try
+            {
+                sw.Write(save);
+                sw.Flush();
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("An error was encountered saving to the selected file");
+                throw;
+            }
+            //and close the writer, but do it on the same level it was opened in case of errors.
+            sw.Close();
+        }//end SaveToFile method.
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
+        {
+            //prompts the user to cancel
+            if (MessageBoxResult.Yes == MessageBox.Show("The program is closing and will automatically save the current contacts, would you like to discard changes since the last save?", "Program Closing", MessageBoxButton.YesNo, MessageBoxImage.Warning))
+            {
+                MessageBox.Show("Autosave cancelled.");
+            }
+            else
+            {
+                SavetoFile("contacts.txt");
+            }
+        }
+
+        private void menuShowAge_Click(object sender, RoutedEventArgs e)
+        {
+           /* if (dgEditor.SelectedItem.GetType() == Contact)
+            {
+                MessageBox.Show(dgEditor.SelectedItem.Age.ToString);
+            }
+            MessageBox.Show(dgEditor.SelectedItem.ToString());*/
         }
     }
 }
