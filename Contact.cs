979c13b3b6d54f4840d6058d0eb4c47f2097522a
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2020-04-19 21:21:23 -0400

    Add files via upload

---
+++ Contact.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _205704Unit2Summative
+{
+    class Contact
+    {
+        public string firstName { get; set; }
+        public string lastName { get; set; }
+        public int yearBorn { get; set; }
+        public int monthBorn { get; set; }
+        public int dayBorn { get; set; }
+        public string email { get; set; }
+        private int age;
+
+        public Contact (string fileLine)
+        {
+            parseLine(fileLine);
+        }
+        private void parseLine (string unparsedLine)
+        {
+            string Line = unparsedLine;
+            firstName = Line.Substring(0, Line.IndexOf(","));
+            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            lastName = Line.Substring(0, Line.IndexOf(","));
+            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int yB);
+            yearBorn = yB;
+            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int mB);
+            monthBorn = mB;
+            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int dB);
+            dayBorn = dB;
+            Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
+            email = Line.Substring(0, Line.IndexOf(","));
+        }
+        private void SetAge()
+        {
+            DateTime birthDay = new DateTime(yearBorn, monthBorn, dayBorn);
+            DateTime dateToday = DateTime.Today;
+            TimeSpan TSage = dateToday - birthDay;
+            age = (TSage.Days) / 1461 / 4;
+        }
+    }
+}
---
+++ MainWindow.xaml.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.IO;
+
+namespace _205704Unit2Summative
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        List<Contact> contacts;
+        public MainWindow()
+        {
+            InitializeComponent();
+            contacts = new List<Contact>();
+            ReadFromFile("contact.txt");
+        }
+
+        private void menuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            bool? dialogResult = ofd.ShowDialog();
+            if ((bool)ofd.ShowDialog())
+            {
+                ReadFromFile(ofd.FileName);
+            }
+        }
+
+        private void menuSave_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        public void ReadFromFile(String fileURI)
+        {
+            try
+            {
+                StreamReader sr = new StreamReader(fileURI);
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    Contact temp = new Contact(line);
+                    contacts.Add(temp);
+                }
+            }
+            catch (Exception)
+            {
+                MessageBox.Show("Error Reading file at URI: " + fileURI);
+                throw;
+            }
+        }
+    }
+}
