09f25ed8ee81bc8458dd5ed6133453e45269fa2a
Aidan McClung <36234902+MysticalWarrior@users.noreply.github.com>
2020-04-20 21:43:56 -0400

    Add files via upload

--- Contact.cs
+++ Contact.cs
@@ -1,8 +1,11 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿/*
+ * Aidan McClung
+ * April 20,2020
+ * A program to edit and save contact info from text files.
+ */
+using System;
+using System.Windows;
+using System.Diagnostics;//enables the MailTo Process.
 
 namespace _205704Unit2Summative
 {
@@ -14,50 +17,88 @@ namespace _205704Unit2Summative
         public int monthBorn { get; set; }
         public int dayBorn { get; set; }
         public string email { get; set; }
-        public int age;
 
+        /// <summary>
+        /// Creates a new contact from a string representation
+        /// </summary>
+        /// <param name="fileLine">
+        /// The string representation of the contact
+        /// </param>
         public Contact(string fileLine)
         {
             parseLine(fileLine);
         }
+
+        /// <summary>
+        /// Creates a new contact with only the birthday set.
+        /// (Used as the base template for a new contact.)
+        /// </summary>
+        /// <param name="birthday">
+        /// The dateTime value to be set as the birthday.
+        /// </param>
+        public Contact(DateTime birthday)
+        {
+            yearBorn = birthday.Year;
+            monthBorn = birthday.Month;
+            dayBorn = birthday.Day;
+        }
+
+        /// <summary>
+        /// Splits a string representation of a contact and sets the values.
+        /// </summary>
+        /// <param name="unparsedLine"></param>
         private void parseLine(string unparsedLine)
         {
-            string[] data = unparsedLine.Split(",");
-            //string Line = unparsedLine;
-            //firstName = Line.Substring(0, Line.IndexOf(","));
+            string[] data = unparsedLine.Split(',');
             firstName = data[0];
-            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            //lastName = Line.Substring(0, Line.IndexOf(","));
             lastName = data[1];
-            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            //int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int yB); 
             int.TryParse(data[2], out int yB); yearBorn = yB;
-            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            //int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int mB); 
             int.TryParse(data[3], out int mB); monthBorn = mB;
-            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            //int.TryParse(Line.Substring(0, Line.IndexOf(",")), out int dB); 
             int.TryParse(data[4], out int dB); dayBorn = dB;
-            //Line = Line.Substring(Line.IndexOf(",") + 1, Line.Length - (Line.IndexOf(",") + 1));
-            //email = Line;
             email = data[5];
-
-            //SetAge();
         }
-        private void SetAge()
+
+        /// <summary>
+        /// A method which returns the current contact's age, and also prompts the user if their birthday is today.
+        /// </summary>
+        public void GetAge(DateTime dateToday)
         {
-            DateTime birthDay = new DateTime(yearBorn, monthBorn, dayBorn);
-            DateTime dateToday = DateTime.Today;
-            TimeSpan TSage = dateToday - birthDay;
-            age = (TSage.Days) / 1461 * 4;
+            //calculates their age from the date today
+            int age = dateToday.Year - yearBorn;
+
+            //Subtracts a year if they haven't yet had their birthday this year
+            if (monthBorn >= dateToday.Month && dayBorn > dateToday.Day) { age--;}
+
+            //notifies the user if it's their birthday and prompts them to send a message.
+            else if (monthBorn == dateToday.Month && dayBorn == dateToday.Day && MessageBoxResult.Yes == MessageBox.Show("It's " + firstName + "'s Birthday Today! They Turned " + age.ToString() + "! \r Would you like to send them a birthday message?", firstName + "'s birthday", MessageBoxButton.YesNo, MessageBoxImage.Exclamation))
+            {
+                //if user said yes, runs the method and doesnt display the age (because an oversaturation of message boxes is the worst.
+                SendBirthdayMessage();
+                return;
+            }
+            
+            //Displays a MessageBox with the age
+            MessageBox.Show(age.ToString(), firstName + lastName + "'s age:");
         }
 
+        /// <summary>
+        /// Converts the Contact to a string representation.
+        /// </summary>
+        /// <returns> A comma seperated string representing the class.</returns>
         public override string ToString()
         {
             string line = firstName + "," + lastName + "," +
                 yearBorn.ToString() + "," + monthBorn.ToString() + "," +
                 dayBorn.ToString() + "," + email + '\r';
             return line;
+        }
+
+        /// <summary>
+        /// Uses the default mail client to open a default birthday message template.
+        /// </summary>
+        private void SendBirthdayMessage()
+        {
+            Process.Start("mailto:" + email + "?subject=Happy%20Birthday!&body=Hope%20you%20have%20a%20good%20day!");
         }
     }
 }
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -1,8 +1,15 @@
-﻿using System;
+﻿/*
+ * Aidan McClung
+ * April 20,2020
+ * A program to edit and save contact info from text files.
+ */
+using System;
 using System.Collections.Generic;
 using System.Windows;
 using System.IO;
-using Microsoft.Win32;
+using Microsoft.Win32;//provides file dialog windows.
+using System.Diagnostics;//provides https usage (&Mailto:)
+using System.Linq;//ofType<Type> for saving
 
 namespace _205704Unit2Summative
 {
@@ -12,40 +19,29 @@ namespace _205704Unit2Summative
     public partial class MainWindow : Window
     {
         List<Contact> contacts;
+        DateTime dateToday;
         public MainWindow()
         {
             InitializeComponent();
+
+            dateToday = DateTime.Today;
+
+            //Reads the program's default saved file and binds it to the editor.
             contacts = new List<Contact>();
             ReadFromFile("contacts.txt");
-
             dgEditor.ItemsSource = contacts;
         }
 
-        private void menuOpen_Click(object sender, RoutedEventArgs e)
-        {
-            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
-            bool? dialogResult = ofd.ShowDialog();
-            if ((bool)dialogResult)
-            {
-                ReadFromFile(ofd.FileName);
-            }
-        }
-
-        private void menuSave_Click(object sender, RoutedEventArgs e)
-        {
-            SaveFileDialog sfd = new SaveFileDialog();
-            bool? fileSelected = sfd.ShowDialog();
-            if ((bool)fileSelected)
-            {
-                SavetoFile(sfd.FileName);
-            }
-        }
-
-        public void ReadFromFile(String fileURI)
+        /// <summary>
+        /// Reads the data from a .txt or .csv file (or a similar type) and imports any saved contacts.
+        /// </summary>
+        /// <param name="fileName">
+        /// The name and path (if not relative) of the file to read from.</param>
+        public void ReadFromFile(String fileName)
         {
             try
             {
-                StreamReader sr = new StreamReader(fileURI);
+                StreamReader sr = new StreamReader(fileName);
                 while (!sr.EndOfStream)
                 {
                     string line = sr.ReadLine();
@@ -56,56 +52,135 @@ namespace _205704Unit2Summative
             }
             catch (Exception)
             {
-                MessageBox.Show("Error Reading file at URI: " + fileURI);
+                MessageBox.Show("Error Reading file: " + fileName);
                 throw;
             }
             dgEditor.ItemsSource = null;
             dgEditor.ItemsSource = contacts;
         }//end read from file method
 
+        /// <summary>
+        /// //attempts to write the string representations of the contacts list to the passed file.
+        /// </summary>
+        /// <param name="fileName">
+        /// The name of the file to save to.
+        /// </param>
         public void SavetoFile(string fileName)
         {
+            //saves the change if the user saves while in editing mode.
+            if (!dgEditor.CommitEdit()) { MessageBox.Show("Please commit edits before saving"); }
+
+            //creates a string and adds the contacts to it.
             string save = "";
-            foreach(Contact c in contacts)
+            foreach (Contact c in dgEditor.Items.OfType<Contact>())
             {
                 save += c.ToString();
             }
 
+            //and saves them to the file.
             StreamWriter sw = new StreamWriter(fileName);
             try
             {
                 sw.Write(save);
+                //makes sure all data is written.
                 sw.Flush();
             }
-            catch (Exception)
+            catch (Exception e)
             {
-                MessageBox.Show("An error was encountered saving to the selected file");
+                MessageBox.Show("An error was encountered saving to the selected file /r/r Specific Error:" + e.ToString());
                 throw;
             }
             //and close the writer, but do it on the same level it was opened in case of errors.
             sw.Close();
-        }//end SaveToFile method.
+        }
 
+        /// <summary>
+        /// Handles confirmation of window closing and change saving.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
-        {
-            //prompts the user to cancel
-            if (MessageBoxResult.Yes == MessageBox.Show("The program is closing and will automatically save the current contacts, would you like to discard changes since the last save?", "Program Closing", MessageBoxButton.YesNo, MessageBoxImage.Warning))
+        {//return;//uncomment to disable messageboxes and autosaving during troubleshooting
+            //prompts the user to cancel autosave or closing
+            MessageBoxResult prompt = MessageBox.Show("The program is closing and will automatically save the current contacts, would you like to *discard* changes since the last save?", "Program Closing", MessageBoxButton.YesNo, MessageBoxImage.Warning);
+            if (prompt == MessageBoxResult.Yes)
             {
-                MessageBox.Show("Autosave cancelled.");
+                MessageBox.Show("Changes discarded"); return;
             }
-            else
+            //Note: Cancelling closing the program was canceled. (change to yesNoCancel to enable)
+            else if (prompt == MessageBoxResult.Cancel)
             {
-                SavetoFile("contacts.txt");
+                //cancels the closing of the program
+                e.Cancel = true;
+            }
+            //To use AutoSave during debugging operations, set this to the project folder path.
+            SavetoFile("contacts.txt");
+        }
+
+        /// <summary>
+        /// Uses a FileDialog to import contacts from a saved file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuOpen_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog ofd = new Microsoft.Win32.OpenFileDialog();
+            bool? dialogResult = ofd.ShowDialog();
+            if ((bool)dialogResult)
+            {
+                ReadFromFile(ofd.FileName);
             }
         }
 
+        /// <summary>
+        /// Opens a FileDialog and saves to a selected file.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuSave_Click(object sender, RoutedEventArgs e)
+        {
+            SaveFileDialog sfd = new SaveFileDialog();
+            bool? fileSelected = sfd.ShowDialog();
+            if ((bool)fileSelected)
+            {
+                SavetoFile(sfd.FileName);
+            }
+        }
+
+        /// <summary>
+        /// Runs the Contact.GetAge method, provided a contact has been selected.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
         private void menuShowAge_Click(object sender, RoutedEventArgs e)
         {
-           /* if (dgEditor.SelectedItem.GetType() == Contact)
-            {
-                MessageBox.Show(dgEditor.SelectedItem.Age.ToString);
-            }
-            MessageBox.Show(dgEditor.SelectedItem.ToString());*/
+            //In order to run the method we have to make sure that the selected item is a contact, otherwise there's errors.
+            Contact temp = (Contact)dgEditor.SelectedItem;
+            if (temp != null) { temp.GetAge(dateToday); }
+            //(date today is a global variable because it is used by other methods.)
+        }
+
+        /// <summary>
+        /// Adds a new contact with today's date as their birthday.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void menuAdd_Click(object sender, RoutedEventArgs e)
+        {
+            contacts.Add(new Contact(dateToday));
+            dgEditor.ItemsSource = null;
+            dgEditor.ItemsSource = contacts;
+        }
+
+        /// <summary>
+        /// Redirects user to the program info file
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="e"></param>
+        private void Help_Click(object sender, RoutedEventArgs e)
+        {
+            //the program info is currently in the github ReadMe.
+            Process.Start("https://raw.githubusercontent.com/MysticalWarrior/205704Unit2Summative/master/README.md");
         }
     }
 }
